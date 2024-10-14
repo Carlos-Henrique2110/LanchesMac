e7986dddbc9b584e27ba20bd8dd0b80c8d711bb9
Carlos Henrique <Carlos Henrique@HENRIQUE>
2024-10-14 11:34:50 -0300

    Add project files.

---
+++ LanchesMac/Models/LancheGrafico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Models
+{
+    public class LancheGrafico
+    {
+        public string LancheNome {get; set;}
+
+        public int LanchesQuantidade {get; set;}
+
+        public decimal LanchesValorTotal {get; set;}
+        
+    }
+}
